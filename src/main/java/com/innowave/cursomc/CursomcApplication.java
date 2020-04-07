d921b0e880e7f016bd922c3d3851407d8ce075b8
Pedro Fernandes <pcruzfernandes12@gmail.com>
2020-04-07 16:46:40 +0100

    Build initial info for the data base

--- src/main/java/com/innowave/cursomc/CursomcApplication.java
+++ src/main/java/com/innowave/cursomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.innowave.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.innowave.cursomc.domain.Category;
+import com.innowave.cursomc.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1  = new Category(null,"Informatics");
+		Category cat2  = new Category(null,"Office");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
