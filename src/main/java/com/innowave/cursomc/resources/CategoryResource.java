5baaf55c8f66a6696d9acdf0f2cde7a12f921865
Pedro Fernandes <pcruzfernandes12@gmail.com>
2020-04-07 14:51:30 +0100

    Create Category Domain

--- src/main/java/com/innowave/cursomc/resources/CategoryResource.java
+++ src/main/java/com/innowave/cursomc/resources/CategoryResource.java
@@ -1,15 +1,28 @@
 package com.innowave.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.innowave.cursomc.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "Rest is working";
+	public List<Category> list() {
+		
+		Category cat1 = new Category(1,"informatics");
+		Category cat2 = new Category(2,"office");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 }
