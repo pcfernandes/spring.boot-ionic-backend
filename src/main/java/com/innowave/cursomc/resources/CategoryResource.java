4cda60f9e483c19c9bf3dec751af5bc3442098b8
Pedro Fernandes <pcruzfernandes12@gmail.com>
2020-05-15 15:10:12 +0100

    Insert Category Service and request

--- src/main/java/com/innowave/cursomc/resources/CategoryResource.java
+++ src/main/java/com/innowave/cursomc/resources/CategoryResource.java
@@ -2,13 +2,13 @@ package com.innowave.cursomc.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.innowave.cursomc.domain.Category;
 import com.innowave.cursomc.services.CategoryService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -23,5 +23,13 @@ public class CategoryResource {
 		Category obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Category obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
