8b4881fb0374a88c5a048dd1323fe619c9ac0171
Pedro Fernandes <pcruzfernandes12@gmail.com>
2020-05-18 10:29:01 +0100

    Exception hadler for delete

--- src/main/java/com/innowave/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/innowave/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -19,7 +19,7 @@ public class ResourceExceptionHandler {
     }
 
     @ExceptionHandler(DataIntegrityException.class)
-    public ResponseEntity<StandardError> dataIntegrity(ObjectNotFoundException e, HttpServletRequest request){
+    public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
         StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
